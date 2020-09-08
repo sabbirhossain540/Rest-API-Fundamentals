442dace077d098150f4d00ddaedaf99ff196f026
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-08 20:13:58 +0600

    initial rest api

---
+++ rapi/app/Model/Sclass.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sclass extends Model
+{
+    protected $fillable = [
+        'class_name',
+    ];
+}
---
+++ rapi/routes/api.php
@@ -0,0 +1,6 @@
+<?php
+
+Route::ApiResource('/class','Api\SclassController');
+
+
+?>
