65c9a424a82ecc4ce6d1fc732de18c9f3c8907a1
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-09 21:10:31 +0600

    JWT-Auth

--- rapi/routes/api.php
+++ rapi/routes/api.php
@@ -6,4 +6,14 @@ Route::ApiResource('/section','Api\SectionController');
 Route::ApiResource('/student','Api\StudentController');
 
 
+Route::group([
+	'prefix' => 'auth'
+], function(){
+	Route::post('login', 'AuthController@login');
+	Route::post('logout', 'AuthController@logout');
+	Route::post('refresh', 'AuthController@refresh');
+	Route::post('me', 'AuthController@me');
+});
+
+
 ?>
