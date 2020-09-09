ddb1a3b340bf505a20ced1e055dc690ff63cc3db
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-09 21:36:38 +0600

    GWT-Token integratiob with laravel

--- rapi/routes/api.php
+++ rapi/routes/api.php
@@ -7,12 +7,16 @@ Route::ApiResource('/student','Api\StudentController');
 
 
 Route::group([
-	'prefix' => 'auth'
-], function(){
-	Route::post('login', 'AuthController@login');
-	Route::post('logout', 'AuthController@logout');
-	Route::post('refresh', 'AuthController@refresh');
-	Route::post('me', 'AuthController@me');
+
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
 });
 
 
