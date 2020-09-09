f616de5be96708bb70416c0277b8c57bd3eaee38
Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-09-09 21:58:25 +0600

    JWT registration System

--- rapi/app/Http/Controllers/AuthController.php
+++ rapi/app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use DB;
 
 class AuthController extends Controller
 {
@@ -14,7 +16,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
     /**
@@ -79,5 +81,22 @@ class AuthController extends Controller
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60
         ]);
+    }
+
+    public function register(Request $request){
+    	$validateData = $request->validate([
+    		'name' => ['required', 'string', 'max:255'],
+    		'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+    		'password' => ['required', 'string', 'min:6' , 'max:255', 'confirmed'],
+    	]);
+
+    	$data = array();
+    	$data['name'] = $request->name;
+    	$data['email'] = $request->email;
+    	$data['password'] = Hash::make($request->password);
+
+    	DB::table('users')->insert($data);
+
+    	return $this->login($request);
     }
 }
--- rapi/routes/api.php
+++ rapi/routes/api.php
@@ -16,6 +16,7 @@ Route::group([
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
+    Route::post('register', 'AuthController@register');
 
 });
 
